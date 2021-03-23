65a7a89cf591fb8343f136f34bb6f8665117ae62
Sagar Barapatre <b119016@iiit-bh.ac.in>
2021-03-23 18:51:50 +0530

    Add files via upload

---
+++ public/js/update-driver.js
@@ -0,0 +1,41 @@
+$("#add_driver").submit(function (event) {
+  alert("Driver Inserted Successfully!");
+});
+
+$("#update_driver").submit(function (event) {
+  event.preventDefault();
+
+  var unindexed_array = $(this).serializeArray();
+  var data = {};
+  unindexed_array.map(function (n, i) {
+    data[n["name"]] = n["value"];
+  });
+  console.log(data);
+
+  var request = {
+    url: `http://localhost:8080/api/drivers/${data.id}`,
+    method: "PUT",
+    data: data,
+  };
+
+  $.ajax(request).done(function (response) {
+    alert("Data Updated Successfully!");
+  });
+});
+
+$ondelete = $(".table td a.delete");
+$ondelete.click(function () {
+  var id = $(this).attr("data-id");
+
+  var request = {
+    url: `http://localhost:8080/api/drivers/${id}`,
+    method: "DELETE",
+  };
+
+  if (confirm("Do you really want to delete this record?")) {
+    $.ajax(request).done(function (response) {
+      alert("Data Deleted Successfully!");
+      location.reload();
+    });
+  }
+});
